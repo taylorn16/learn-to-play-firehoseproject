f62ae248bb0e0f32d4ce652f219247af24795b17
Nick Taylor <taylorn@udel.edu>
2016-02-26 15:59:37 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LearnToPlay::Application.config.secret_key_base = 'aaf54f9e5cd4d9d12515db83b414efd1abf6d19dd91fe355d3f4459e1a24c3202643275e8e86181adac30b14a7b7aadd6cfd5c6a8a315aecb0836bbc25567645'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LearnToPlay::Application.config.session_store :cookie_store, key: '_learn-to-play_session'
