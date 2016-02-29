3ca59f9502823ca7ed840b3d3d0e1a2a8e483abb
Nick Taylor <taylorn@udel.edu>
2016-02-29 17:51:18 +0000

    Added courses controller for unauthenticated users

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ LearnToPlay::Application.routes.draw do
   devise_for :users
   root 'static_pages#index'
 
+  resources :courses, :only => [:index, :show]
+
   namespace :instructor do
     # Directs /instructor/courses/* to Instructor::CoursesController
     resources :courses, :only => [:new, :create, :show]
