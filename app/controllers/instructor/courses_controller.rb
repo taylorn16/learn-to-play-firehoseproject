3e242730889e3db5174a4be1371da5cae75d1b98
Nick Taylor <taylorn@udel.edu>
2016-03-05 21:53:48 +0000

    Secured lessons, sections, and courses

--- app/controllers/instructor/courses_controller.rb
+++ app/controllers/instructor/courses_controller.rb
@@ -1,6 +1,7 @@
 class Instructor::CoursesController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :authorize_current_course, only: [:show]
 
   def new
     @course = Course.new
@@ -14,15 +15,23 @@ class Instructor::CoursesController < ApplicationController
     else
       return render :new, status: :unprocessable_entity
     end
-
-
   end
 
   def show
-    @course = Course.find(params[:id])
   end
 
   private
+
+  def authorize_current_course
+    unless current_course.user == current_user
+      return render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:id])
+  end
 
   def course_params
     params.require(:course).permit(:title, :description, :cost)
--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -1,20 +1,30 @@
 class Instructor::SectionsController < ApplicationController
 
   before_action :authenticate_user!
+  before_action :authorize_current_course
 
   def new
-    @course = Course.find(params[:course_id])
     @section = Section.new
   end
 
   def create
-    @course = Course.find(params[:course_id])
-    @section = @course.sections.create(section_params)
+    @section = current_course.sections.create(section_params)
 
-    return redirect_to instructor_course_path(@course)
+    return redirect_to instructor_course_path(current_course)
   end
 
   private
+
+  def authorize_current_course
+    unless current_course.user == current_user
+      return render text: "Unauthorized", status: :unauthorized
+    end
+  end
+
+  helper_method :current_course
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
 
   def section_params
     params.require(:section).permit(:title)
