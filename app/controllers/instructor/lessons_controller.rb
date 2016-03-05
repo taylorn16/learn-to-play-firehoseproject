c4d98d8f513d5321d28c75fdd961b1526f2a6fe7
Nick Taylor <taylorn@udel.edu>
2016-03-05 21:34:32 +0000

    Refactored lessons controller

--- app/controllers/instructor/lessons_controller.rb
+++ app/controllers/instructor/lessons_controller.rb
@@ -1,17 +1,30 @@
 class Instructor::LessonsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :authorize_current_section
+
   def new
-    @section = Section.find(params[:section_id])
     @lesson = Lesson.new
   end
 
   def create
-    @section = Section.find(params[:section_id])
-    @lesson = @section.lessons.create(lesson_params)
+    @lesson = current_section.lessons.create(lesson_params)
 
-    return redirect_to instructor_course_path(@section.course)
+    return redirect_to instructor_course_path(current_section.course)
   end
 
   private
+
+  def authorize_current_section
+    unless current_section.course.user == current_user
+      return render({:text => 'Unauthorized'}, :status => :unauthorized)
+    end
+  end
+
+  helper_method :current_section
+  def current_section
+    @current_section ||= Section.find(params[:section_id])
+  end
 
   def lesson_params
     params.require(:lesson).permit(:title, :subtitle)
