9b8e98c3dce000c0f8f6b11aed2b09ca732c15b1
Nick Taylor <taylorn@udel.edu>
2016-03-05 22:19:47 +0000

    Finished CRUD for lessons

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,13 @@ LearnToPlay::Application.routes.draw do
 
   resources :courses, :only => [:index, :show]
 
+  resources :lessons, :only => [:show]
+
   namespace :instructor do
     resources :sections, :only => [] do
       resources :lessons, :only => [:new, :create]
     end
-    
+
     resources :courses, :only => [:new, :create, :show] do
       resources :sections, :only => [:new, :create]
     end
