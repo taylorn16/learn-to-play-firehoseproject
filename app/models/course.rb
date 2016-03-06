943447cb8a8f00f890c036123ea343e4fc28faca
Nick Taylor <taylorn@udel.edu>
2016-03-06 20:32:15 +0000

    Adjusted image upload sizes

--- app/models/course.rb
+++ app/models/course.rb
@@ -9,6 +9,6 @@ class Course < ActiveRecord::Base
   validates :cost, presence: true, numericality: {greater_than_or_equal_to: 0}
 
   # Paperclip
-  has_attached_file :course_photo, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+  has_attached_file :course_photo, styles: { banner: "800x350#", thumb: "100x100#" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :course_photo, content_type: /\Aimage\/.*\Z/
 end
