8a5757c65db4ab9f4f295aeef7db473b2c006674
Nick Taylor <taylorn@udel.edu>
2016-03-14 16:23:49 +0000

    Finished first draft of enrollments (no pay gate yet)

--- app/controllers/enrollments_controller.rb
+++ app/controllers/enrollments_controller.rb
@@ -1,9 +1,27 @@
 class EnrollmentsController < ApplicationController
 
-  
+  before_action :authenticate_user!
 
   def create
+    enrollment = Enrollment.new
+    enrollment.course_id = current_course.id
+    enrollment.user_id = current_user.id
 
+    if enrollment.save
+      return redirect_to course_path(current_course)
+    else
+      return render text: "Enrollment didn't save properly.", status: :internal_server_error
+    end
+  end
+
+  private
+
+  def current_course
+    @current_course ||= Course.find(params[:course_id])
+  end
+
+  def enrollment_params
+    params.require(:enrollment).permit(:course_id, :user_id)
   end
 
 end
