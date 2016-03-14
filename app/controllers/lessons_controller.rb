2df9bc9b33fcc34ea279bd05f0912030e80691d6
Nick Taylor <taylorn@udel.edu>
2016-03-14 16:44:08 +0000

    Secured lesson show page from unenrolled users

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,8 +1,20 @@
 class LessonsController < ApplicationController
+
+  before_action :authenticate_user!
+  before_action :authorize_view_lesson, only: [:show]
+
   def show
   end
 
   private
+
+  def authorize_view_lesson
+    current_course = current_lesson.section.course
+    unless current_user.enrolled_in?(current_course)
+      flash[:error] = "You must enroll in this course to view its lessons!"
+      redirect_to course_path(current_course)
+    end
+  end
 
   helper_method :current_lesson
   def current_lesson
